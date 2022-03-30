6ff2f2bc4a8eec09cb14fcff23e86b0c56db3cb7
Ankit Kumar <ankit.kumar@financialforce.com>
2022-03-30 10:08:39 +0530

    Added functional component

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,69 +1,59 @@
-import { Component } from "react";
-import { Card, CardImg, CardText, CardBody,
-    CardTitle} from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+function RenderDish({ dish }) {
+  if (dish != null)
+    return (
+      <Card>
+        <CardImg top src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    );
+  else {
+    return <div></div>;
+  }
+}
 
-class DishDetail extends Component {
-    renderDish(dish) {
-        if (dish != null)
-            return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else {
-            return(
-                <div></div>
-            );
-        }
-    }
+function options() {
+  return {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+  };
+}
 
-    options() {
-        return {
-            year: 'numeric', month: 'short', day: '2-digit'
-          };
-    }
-    /**
-     * Method to render comments on a particular selected dish
-     * @param {*} dish Selected Dish
-     * @returns the view showing all of the comments
-     */
-    renderComments(dish) {
-        if(dish) {
-            return (
-                dish.comments.map(element => {
-                        return(
-                            <ul key={element.id} className = "list-unstyled">
-                                <li>{element.comment}</li>
-                                <li>-- {element.author}, {new Intl.DateTimeFormat('en-US', this.options()).format(new Date(Date.parse(element.date)))}</li>
-                            </ul>  
-                        )
-                    }
-                )
-            )
-        }
-        else{
-            return(
-                <div></div>
-            )
-        }
-    }
-    render() {
-        return (
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                  {this.renderDish(this.props.dish)}
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    <h4>Comments</h4>
-                    {this.renderComments(this.props.dish)}
-                </div>
-            </div>
-        )
-    }
+function RenderComments({ comments }) {
+  if (comments) {
+    return comments.map((element) => {
+      return (
+        <ul key={element.id} className="list-unstyled">
+          <li>{element.comment}</li>
+          <li>
+            -- {element.author},{" "}
+            {new Intl.DateTimeFormat("en-US", options()).format(
+              new Date(Date.parse(element.date))
+            )}
+          </li>
+        </ul>
+      );
+    });
+  } else {
+    return <div></div>;
+  }
+}
+function DishDetail(props) {
+  return (
+    <div className="row">
+      <div className="col-12 col-md-5 m-1">
+        <RenderDish dish={props.dish} />
+      </div>
+      <div className="col-12 col-md-5 m-1">
+        <h4>Comments</h4>
+        <RenderComments comments={props.dish.comments} />
+      </div>
+    </div>
+  );
 }
 
 export default DishDetail;
